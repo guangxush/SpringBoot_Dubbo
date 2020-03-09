6a45131d653896deab42fcfa3e6f3a6cea9a65c4
guangxush <guangxush@163.com>
2020-03-09 21:02:50 +0800

    service

--- dubbo-provider/src/main/java/com/shgx/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/shgx/provider/DubboProviderApplication.java
@@ -1,6 +1,5 @@
 package com.shgx.provider;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
